95dd3a0fbb26ed1b39e11c5b1e48ce9738debdb5
SofiaFia03 <74136529+SofiaFia03@users.noreply.github.com>
2021-01-08 19:14:09 -0800

    Add files via upload

---
+++ Inheritance/src/com/dicoding/javafundamental/inheritance/Hewan.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USER
+ */
+    package com.dicoding.javafundamental.inheritance;
+     
+    public class Hewan {
+    public Hewan() {
+        System.out.println("construct Hewan");
+    }
+ 
+    public void makan() {  // base method
+        System.out.println("Hewan sedang makan..");
+    }
+}
